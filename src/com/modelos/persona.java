a198f1fcd46293ae315b7507d5b8193ec4af7c5a
danie <danie@LAPTOP-TKHGRHUV>
2023-05-12 14:18:36 -0600

    angel gabriel jimenez rodriguez

---
+++ src/com/modelos/persona.java
@@ -0,0 +1,51 @@
+
+package com.modelos;
+
+/**
+ *
+ * @author danie
+ */
+public class persona {
+    private String nombre;
+    private String correo;
+    private int edad;
+   
+
+    public persona() {
+    }
+
+    public persona(String nombre, int edad, String correo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+    
+    
+    
+}
